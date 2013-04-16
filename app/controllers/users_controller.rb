885c772cf5f6b072896c53e9034a057f1403d7c8
Timothy Sikes <timothy.sikes@wq0183jcatmm.oc.edu>
2013-04-16 10:49:42 -0500

    Unknown amount of changes
    From Chapter 6 after.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+  
   def show
     @user = User.find(params[:id])
   end
@@ -11,10 +15,54 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @users = User.all
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+  
+  def index
+    @users = User.all
+  end
+  
+  private
+  
+  def signed_in_user
+    redirect_to signin_path, notice: "Please sign in" unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
+  def admin_user
+      redirect_to(root_path) unless current_user.admin?
   end
 end
