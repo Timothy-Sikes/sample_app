1774c968295811a4512b93850bfe55c5b37347b7
Timothy Sikes <tsikes64@gmail.com>
2013-04-18 10:49:01 -0500

    Just starting out

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ActiveRecord::Base
   attr_accessible :content, :course_id, :rating, :user_id
+  
+  belongs_to :user
+  belongs_to :course
+  
+  validates :rating, presence: true :inclusion => { :in => 0..5 }
+  validates :user_id, presence: true
+
+  default_scope order: 'microposts.created_at DESC'
 end
